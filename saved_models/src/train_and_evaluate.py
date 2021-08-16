449b6ef7943200975640291fa351a68252f006f7
StoparM <matevz.stopar@gmail.com>
2021-08-16 14:12:26 +0200

    removed plots

--- saved_models/src/train_and_evaluate.py
+++ saved_models/src/train_and_evaluate.py
@@ -86,15 +86,15 @@ def train_and_evaluate(config_path):
 
     print(classification_report(y_test, predicted_val))
 
-    cm = confusion_matrix(y_test, predicted_val)
-    print(cm)
+    # cm = confusion_matrix(y_test, predicted_val)
+    # print(cm)
 
         
-    df1 = pd.DataFrame(predicted_val, columns = ['Predicted'])
-    df_cm = pd.concat([y_test, df1], axis=1)
-    print(df_cm)
+    # df1 = pd.DataFrame(predicted_val, columns = ['Predicted'])
+    # df_cm = pd.concat([y_test, df1], axis=1)
+    # print(df_cm)
         
-    df_cm.to_csv('cm.csv', index = False)
+    # df_cm.to_csv('cm.csv', index = False)
 
     roc_auc = roc_auc_score(y_test, model.predict_proba(x_test)[:, 1])
     print('ROC_AUC:{0:0.2f}'.format(roc_auc))
@@ -109,7 +109,7 @@ def train_and_evaluate(config_path):
         scores = {
             "train_score": train_score,
             "test_score": test_score,
-            "roc_auc": roc_auc,
+            # "roc_auc": roc_auc,
             "Logistic Accuracy": Logistic_Accuracy      
         }
         json.dump(scores, f, indent=4)

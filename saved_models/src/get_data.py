706c67bfba2b1b942d3d39edb9ba7c4b560533e2
StoparM <matevz.stopar@gmail.com>
2021-09-23 12:25:21 +0200

    s3 deleted

--- saved_models/src/get_data.py
+++ saved_models/src/get_data.py
@@ -10,7 +10,7 @@ def read_params(config_path):
 
 def get_data(config_path):
     config = read_params(config_path)
-    data_path = config["data_source"]["s3_source"]
+    data_path = config["data_source"]["source"]
 
     df = pd.read_csv(data_path, header=None)
     df = df.replace('NiAnomalije', 0)
